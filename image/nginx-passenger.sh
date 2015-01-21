733e8d0fa87793096d55a752e409da3e9b7cd9ee
Mike Petersen <mike@odania-it.de>
2015-01-21 14:48:35 +0100

    Added RAILS_ENV variable to allowed env vars in nginx

--- image/nginx-passenger.sh
+++ image/nginx-passenger.sh
@@ -16,6 +16,7 @@ cp /build/config/nginx_main_d_default.conf /etc/nginx/main.d/default.conf
 cp /build/config/mysql-env.conf /etc/nginx/main.d/mysql-env.conf
 cp /build/config/redis-env.conf /etc/nginx/main.d/redis-env.conf
 cp /build/config/elasticsearch-env.conf /etc/nginx/main.d/elasticsearch-env.conf
+cp /build/config/rails-env.conf /etc/nginx/main.d/rails-env.conf
 
 ## Install Nginx runit service.
 mkdir /etc/service/nginx
