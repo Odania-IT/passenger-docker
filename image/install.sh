11cc3c0c9fcf2bc034dd86adb6a50a5ac98dc08f
Mike Petersen <mike@odania-it.de>
2015-01-22 14:37:58 +0100

    Added background worker and using env var to choose

--- image/install.sh
+++ image/install.sh
@@ -13,4 +13,14 @@ set -x
 # Must be installed after Ruby, so that we don't end up with two Ruby versions.
 /build/nginx-passenger.sh
 
+# Setup background_worker service
+/build/background_worker.sh
+
+# Copy startscript
+mkdir -p /etc/my_init.d
+cp /build/my_init.d/select_type.sh /etc/my_init.d/select_type.sh
+
+# Disable ssh
+touch /etc/service/sshd/down
+
 /build/finalize.sh
